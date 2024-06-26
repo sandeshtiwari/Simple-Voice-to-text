8b919bd5f022d9dc08c05d7cfc280ec70965c0f1
stiwari64 <sandeshtiwari@live.com>
2024-06-26 09:34:47 -0500

    simple app completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 
 function App() {
+  const [stateTranscript, setStateTranscript] = useState(null);
+  const { transcript, resetTranscript } = useSpeechRecognition()
+
+  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+    return null
+  }
+  const populateTranscript = () => {
+    SpeechRecognition.startListening();
+    setStateTranscript(transcript)
+    // console.log(transcript)
+    console.log(stateTranscript)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={populateTranscript}>Start</button>
+      <button onClick={SpeechRecognition.stopListening}>Stop</button>
+      <button onClick={resetTranscript}>Reset</button>
+      <p>{transcript}</p>
     </div>
-  );
+  )
 }
 
 export default App;
